1b01ed85ea8300d2e300ed296af25dd095b27f58
j7breuer <66193217+j7breuer@users.noreply.github.com>
2021-08-25 23:48:29 -0400

    Updated prodigy pattern creator with argparse and cleaned up code

--- nlp/classification/data_cleaning/prodigy_pattern_creator.py
+++ nlp/classification/data_cleaning/prodigy_pattern_creator.py
@@ -2,11 +2,33 @@
 import json
 import pandas as pd
 
+#------------#
+# Parse Args #
+#------------#
+arg_parser = argparse.ArgumentParser(description="Data arguments")
+arg_parser.add_argument(
+    "inpt",
+    type = "str",
+    help = "Filename of input csv located in inpt folder with extension"
+)
+arg_parser.add_argument(
+    "oupt",
+    type = "str",
+    help = "Name of output JSONL file located in the oupt folder"
+)
+args = arg_parser.parse_args()
+
+# Run
 if __name__ in "__main__":
-    df = pd.read_csv("../data/inpt/tweet_classification_patterns.csv", encoding = "utf-8")
+    # Load in CSV of tweet classification patterns for prodigy's text classification learning recipe
+    df = pd.read_csv(f"../data/inpt/{args.inpt}", encoding = "utf-8")
+    # Create list to append to JSONL file
     oupt_list = []
+    # Loop through df
     for index, row in df.iterrows():
+        # Create dict to add to list
         oupt_list.append({"label": row['label'], "pattern":[{"lower": row['text']}]})
-    with open("../data/oupt/tweet_classification_patterns.jsonl", 'w', encoding='utf-8') as f:
+    # Save as JSONL file
+    with open(f"../data/oupt/{args.oupt}", 'w', encoding='utf-8') as f:
         for item in oupt_list:
             f.write(json.dumps(item, ensure_ascii=False) + "\n")
